cbe5ae40d1d691d9386f499ff6682e489356c42b
RamaIs2 <rama.garciae86@gmail.com>
2021-09-01 16:11:59 -0300

    Finalizado el programa

--- cuenta.php
+++ cuenta.php
@@ -4,3 +4,14 @@ $horas  = $_GET['horas']
 
 $Pporhora = $sueldo/$horas
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TP3</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Paga por hora </h1>
+<?php> echo "<p>Se te paga la hora = $Pporhora </p>"; ?>
+</body>
+</html>
