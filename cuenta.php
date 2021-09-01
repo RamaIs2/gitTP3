43cbef68ec47c69caa391608fac25941a9954643
RamaIs2 <rama.garciae86@gmail.com>
2021-09-01 16:24:40 -0300

    Corrección del codigo para finalizarlo

--- cuenta.php
+++ cuenta.php
@@ -1,17 +1,17 @@
 <?php
-$sueldo = $_GET['sueldo']
-$horas  = $_GET['horas']
+$sueldo = $_GET['sueldo'];
+$horas  = $_GET['horas'];
 
-$Pporhora = $sueldo/$horas
+$Pporhora = $sueldo/$horas;
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>TP3</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-<h1>Paga por hora </h1>
-<?php> echo "<p>Se te paga la hora = $Pporhora </p>"; ?>
-</body>
+    <head>
+     <title>TP3hora</title>
+     <meta charset="UTF-8">
+    </head>
+    <body>
+     <h1>Paga por hora </h1>
+     <?php echo "<p>Se te paga la hora = $Pporhora </p>"; ?>
+    </body>
 </html>
